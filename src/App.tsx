467c4aabeea527be6af09c75e543e9c38a8a4846
Lucas Hideki <ls_hdk@protonmail.com>
2020-09-23 14:17:12 -0300

    pagina-login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import SignIn from './pages/Signin/index';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <h1>Hello World</h1>
+    <SignIn />
   </>
 );
 export default App;
